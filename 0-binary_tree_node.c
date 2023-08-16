6a6feadfcc742674ae730547df97ab72dc5107d2
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-16 13:38:24 -0700

    Memory

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,8 +9,11 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
+
 	new = malloc(sizeof(binary_tree_t));
 	new->n = value;
 	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 	return (new);
 }
