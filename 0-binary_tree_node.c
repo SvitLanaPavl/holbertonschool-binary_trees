40113457d0345f8fdbe773e22da1a1f154b14492
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-16 13:32:20 -0700

    first push

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a binary tree
+ * Description: creates a binary tree
+ * @parent: pointer to the parent node
+ * @value: value
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL;
+	new = malloc(sizeof(binary_tree_t));
+	new->n = value;
+	new->parent = parent;
+	return (new);
+}
