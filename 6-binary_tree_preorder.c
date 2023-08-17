547d01976ace18d0ea15b7b897810c16a90012fd
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 11:13:23 -0700

    Preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,8 +8,8 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
+	if (!tree || !func)
 		return;
-	binary_tree_preorder(tree->left, printf("%d ->", tree->n);
-	binary_tree_preorder(tree->right, printf("%d ->", tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
