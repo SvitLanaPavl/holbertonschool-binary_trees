e9d97fd302bb19b04f2187fe3aa1b4af892fe994
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 15:55:49 -0700

    size more broken into parts

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,8 +10,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size = 0;
 
 	if (!tree)
-		return (0);
-	else
-		size = binary_tree_size(tree->left) + binary_tree_size(tree->left) + 1;
+		return (size);
+	size += 1;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->left);
 	return (size);
 }
