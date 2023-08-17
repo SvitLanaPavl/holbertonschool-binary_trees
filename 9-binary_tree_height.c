0f2dccbeb3f9e53c0b7c3e95616c8043dbba27ab
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 12:24:01 -0700

    +1 moved to the recursion from the return

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,10 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
 	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+		return (left_height);
+	return (right_height);
 }
