dac07a577297ab5901f710681cab7704a38ab779
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 09:19:55 -0700

    rearranged

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,16 +8,17 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = NULL;
+	binary_tree_t *new;
 
-	if (!parent || !new)
+	if (!parent)
 		return (NULL);
 	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
 	while (parent->left)
 	{
-		parent->left->parent = new;
 		new->left = parent->left;
-
+		parent->left->parent = new;
 	}
 	parent->left = new;
 return (new);
