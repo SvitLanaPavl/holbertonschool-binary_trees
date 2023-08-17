699e17ef01c5dad14f521d271a17c27481d31418
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 15:03:19 -0700

    Size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * Description: measures the size of a binary tree
+ * @tree: tree
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->left));
+}
