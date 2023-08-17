614af039b46014b200d1fcfceb98718ed9afa32a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 12:33:56 -0700

    DEpth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * Description: measures the depth of a node in a binary tree
+ * @tree: tree
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t left_depth, right_depth;
+
+	if (!tree)
+		return (0);
+	left_depth = binary_tree_depth(tree->left) + 1;
+	right_depth = binary_tree_depth(tree->right) + 1;
+	if (left_depth > right_depth)
+		return (left_depth);
+	return (right_depth);
+}
