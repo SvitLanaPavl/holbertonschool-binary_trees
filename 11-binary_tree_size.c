73b7f56d126941289e2411dd2ebb1ad4936ab09d
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 15:10:11 -0700

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,5 +9,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->left));
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->left) + 1);
 }
