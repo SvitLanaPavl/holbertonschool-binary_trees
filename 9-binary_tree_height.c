172980b8a2c528f3b733f29973cbc3b359804262
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 11:54:10 -0700

    Removed + 1 from max

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,20 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height, max;
+	int left_height, right_height;
 
 	if (!tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
-	{
-		max = left_height + 1;
-		return (max);
-	}
+		return (left_height);
 	else
-	{
-		max = right_height + 1;
-		return (max);
-	}
+		return (right_height);
 }
