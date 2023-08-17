f30970d14245aad808610160c76516e4eadcfb2f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 16:40:59 -0700

    Stupid typo!

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,9 +9,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 	left_height = binary_tree_size(tree->left);
-	right_height = binary_tree_size(tree->left);
+	right_height = binary_tree_size(tree->right);
 	return (left_height + 1 + right_height);
 }
