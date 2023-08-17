1b54f5b6bcdda33b5fcf090a4c502948d67c5381
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 12:25:41 -0700

    One more condition added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
 	left_height = binary_tree_height(tree->left) + 1;
 	right_height = binary_tree_height(tree->right) + 1;
 	if (left_height > right_height)
