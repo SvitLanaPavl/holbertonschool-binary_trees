dbe6f21da3f01e5d23c904423224f6fc3b4078d2
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 12:19:15 -0700

    Data type int to size_t

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,25 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t left_height, right_height;
 
 	if (!tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (max(right_height, left_height) + 1);
-}
-/**
- * max - returns the max number
- * Description: max number
- * @a: first number
- * @b: second number
- * Return: greter number
- */
-int max(int a, int b)
-{
-	if (a > b)
-		return (a);
+	if (left_height > right_height)
+		return (left_height + 1);
 	else
-		return (b);
+		return (right_height + 1);
 }
