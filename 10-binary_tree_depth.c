968c14425981ff57569abafb59e321921fddbb57
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 14:49:41 -0700

    Some refactoring

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,5 +13,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	left_depth = binary_tree_depth(tree->left);
 	right_depth = binary_tree_depth(tree->right);
-	return ((left_depth > right_depth) ? left_depth + 1 : right_depth + 1)
+	return ((left_depth > right_depth) ? left_depth + 1 : right_depth + 1);
 }
