9fffd0bb1fb8ca904967e217f5bea8d982719761
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 16:52:18 -0700

    Betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,11 +8,12 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
+
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
 		count = 1;
-	count += binary_tree_nodes(tree->left); 
+	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
 	return (count);
 }
