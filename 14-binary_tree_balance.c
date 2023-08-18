b9a48c3c2536cbbee024a6fcd62e2052cf9bae69
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 17:16:32 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,3 +15,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 	right = binary_tree_height(tree->right);
 	return (left - right);
 }
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * Description: measures the height of a binary tree
+ * @tree: tree
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
