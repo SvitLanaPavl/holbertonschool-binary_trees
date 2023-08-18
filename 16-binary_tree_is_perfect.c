b2a56640d13a3665358d93e96e42cea87f5a232f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-18 15:15:52 -0700

    Added helper

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,7 +12,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(right->left);
+	right_height = binary_tree_height(tree->right);
 	if (left_height != right_height)
 		return (0);
 	return (binary_tree_is_perfect(tree->left) &&
