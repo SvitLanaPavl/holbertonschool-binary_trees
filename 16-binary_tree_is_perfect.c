a988478b9b0a3cdd3eac37d99110a568cad7196a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-18 15:14:43 -0700

    Added helper

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,13 +7,34 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
+	if (!tree)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(right->left);
+	if (left_height != right_height)
+		return (0);
+	return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * Description: measures the height of a binary tree
+ * @tree: tree
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
-		return (1);
-	if (!tree->left || !tree->right)
 		return (0);
-return (binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right));
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
 }
-
