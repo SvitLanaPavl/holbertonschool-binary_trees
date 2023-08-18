e0395a3310c29b1225ac40cd178bc685e7c81bc0
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-18 16:03:45 -0700

    Uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * Description: finds the uncle of a node
+ * @node: node
+ * Return: pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, grandparent;
+
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	if (parent == grandparent->left)
+		return (grandparent->right)
+	else
+		return (grandparent->left);
+}
