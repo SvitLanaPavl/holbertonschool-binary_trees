bd8d58a926b068a53e2bb8a0e8c7e6ee215c6741
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-18 14:49:12 -0700

    Is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * Description: checks if a binary tree is perfect
+ * @tree: tree
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (!tree->left || !tree->right)
+		return (0);
+return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+}
