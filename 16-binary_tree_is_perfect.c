71bf8635a6a9843af31201a30a8fc1b28f78460c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-18 16:19:49 -0700

    Size added

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,6 +8,7 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left_height, right_height;
+	size_t left_size, right_size;
 
 	if (!tree)
 		return (0);
@@ -16,6 +17,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height != right_height)
+		return (0);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	if (left_size != right_size)
 		return (0);
 	if (binary_tree_is_perfect(tree->left) &&
 		binary_tree_is_perfect(tree->right))
@@ -41,4 +46,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (left_height > right_height)
 		return (left_height);
 	return (right_height);
+}
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * Description: measures the size of a binary tree
+ * @tree: tree
+ * Return: 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+	left_height = binary_tree_size(tree->left);
+	right_height = binary_tree_size(tree->right);
+	return (left_height + 1 + right_height);
 }
