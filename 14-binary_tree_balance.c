eefade159926ea492add670673f2c3827756d7ad
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-08-17 17:11:36 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left = 0, right = 0;
+
 	if (!tree)
 		return (0);
-	return (binary_tree_balance(tree->left) - binary_tree_balance(tree->right));
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
 }
